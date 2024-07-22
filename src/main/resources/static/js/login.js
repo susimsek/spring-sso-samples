c564a4d26d6fd4bca2c4add736de8d492590ac13
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-07-22 20:37:00 +0300

    init commit

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,19 +2,18 @@
 function togglePasswordVisibility() {
   const passwordField = document.getElementById("password");
   const toggleIcon = document.getElementById("toggleIcon");
-  const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
-  passwordField.setAttribute("type", type);
+  const isPassword = passwordField.getAttribute("type") === "password";
+  passwordField.setAttribute("type", isPassword ? "text" : "password");
   toggleIcon.classList.toggle("fa-eye");
   toggleIcon.classList.toggle("fa-eye-slash");
 }
 
 // Show loading spinner and disable submit button on form submit
-function disableSubmitButton() {
+function disableSubmitButton(event) {
   const submitButton = document.getElementById("submitButton");
   submitButton.disabled = true;
   submitButton.querySelector('.spinner-border').style.display = 'inline-block';
   submitButton.querySelector('.sr-only').style.display = 'inline-block';
 }
 
-document.getElementById('loginForm').addEventListener(
-  'submit', disableSubmitButton);
+document.getElementById('loginForm').addEventListener('submit', disableSubmitButton);
