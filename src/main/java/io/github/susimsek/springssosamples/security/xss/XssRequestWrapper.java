8cd3cab3c35b5672f60fdf6fc3b0950c6069b4bd
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-07-26 17:07:01 +0300

    init commit

--- src/main/java/io/github/susimsek/springssosamples/security/xss/XssRequestWrapper.java
+++ src/main/java/io/github/susimsek/springssosamples/security/xss/XssRequestWrapper.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
